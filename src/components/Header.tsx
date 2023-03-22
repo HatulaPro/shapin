88f6d44732984c33145ef29fbd2d233a352e017f
HatulaPro <shalev280804@gmail.com>
2023-03-22 22:45:57 +0200

    Adding home button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { useClerk } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import { CustomSignIn } from "./CustomSignIn";
 import { CustomSignUp } from "./CustomSignUp";
 import { ProfileImage } from "./ProfileImage";
@@ -7,6 +9,12 @@ export const Header = () => {
   const { user } = useClerk();
   return (
     <div className="sticky top-0 left-0 z-[999999] flex w-full gap-2 bg-[#121232] px-6 py-4 text-white">
+      <Link href="/" className="hover:underline">
+        <div className="flex items-center gap-2">
+          <Image src="/logo.png" width={40} height={40} alt="Logo of Shapin" />
+          <span className="hidden text-xl sm:block">Shapin</span>
+        </div>
+      </Link>
       <div className="flex w-full items-center justify-end gap-2">
         {user ? (
           <>
